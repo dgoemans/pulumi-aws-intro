ff00de4e04ad64ae261dc2911eccc2d0f9576739
David Goemans <dgoemans@gmail.com>
2021-08-23 14:16:41 +0200

    Certificate creation and validation

--- infra/index.ts
+++ infra/index.ts
@@ -2,8 +2,35 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const DOMAIN = 'MY-DOMAIN'
+const ENVIRONMENT = pulumi.getStack();
+const PROJECT_NAME = `pulumi-aws-intro-${ENVIRONMENT}`;
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const hostedZone = new aws.route53.Zone(`${DOMAIN}`, {
+  name: DOMAIN
+})
+
+const eastRegion = new aws.Provider("east", {
+  profile: aws.config.profile,
+  region: "us-east-1", // Per AWS, ACM certificate must be in the us-east-1 region.
+});
+
+const certificate = new aws.acm.Certificate(`${PROJECT_NAME}-certificate`, {
+  domainName: DOMAIN,
+  validationMethod: "DNS",
+  subjectAlternativeNames: [`*.${DOMAIN}`],
+}, { provider: eastRegion });
+
+const certificateValidationDomain = new aws.route53.Record(`${DOMAIN}-validation`, {
+  zoneId: hostedZone.id,
+  name: certificate.domainValidationOptions[0].resourceRecordName,
+  type: certificate.domainValidationOptions[0].resourceRecordType,
+  records: [certificate.domainValidationOptions[0].resourceRecordValue],
+  ttl: 60 * 10, // 10 minutes
+});
+
+new aws.acm.CertificateValidation("certificateValidation", {
+  certificateArn: certificate.arn,
+  validationRecordFqdns: [certificateValidationDomain.fqdn],
+}, { provider: eastRegion });
+
